d7551c032de3813fff9b53c2c586690543b25059
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-05-09 01:12:28 -0300

    add boton descarga

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -28,31 +28,27 @@ export class DashboardComponent implements OnInit{
     this.obtenerPedidos();
   }
   obtenerPedidos(): void {
-    this.isLoading = true; // Comenzar la carga
+    this.isLoading = true;
     this.dashboardService.obtenerPedidos().subscribe({
-      next:(data: IPedidosData) => {
-        console.log('Datos recibidos del dashboard:', data);
-        // Asegurarse de que todos los arrays existan incluso si la API no los devuelve
+      next: (data: any) => {
+        // Adaptar los datos recibidos para que todo lo que venga en results vaya a entregados
         this.pedidosData = {
-          pendientes: data.pendientes || [],
-          aprobados: data.aprobados || [],
-          entregados: data.entregados || []
+          pendientes: [],
+          aprobados: [],
+          entregados: data.results || []
         };
         this.setActiveTab(this.activeTab);
-        this.isLoading = false; // Finalizar la carga
-      }, 
+        this.isLoading = false;
+      },
       error: (error) => {
         console.error('Error al obtener pedidos:', error);
-        
         if (error.error && error.error.detail === 'Given token not valid for any token type') {
-          this.toastr.info(
-            'Su sesión ha expirado. Debe iniciar sesión nuevamente'
-          );
-          this.authService.logout();        } else {
-          // Mostrar un mensaje de error más genérico para otros errores
+          this.toastr.info('Su sesión ha expirado. Debe iniciar sesión nuevamente');
+          this.authService.logout();
+        } else {
           this.toastr.error('No se pudieron cargar los pedidos. Intente nuevamente más tarde.');
         }
-        this.isLoading = false; // Finalizar la carga incluso si hay error
+        this.isLoading = false;
       },
     });
   }
