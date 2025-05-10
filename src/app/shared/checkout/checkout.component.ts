dd5297eaab88c7c3ad61b748e79f34a6ad200631
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-05-10 19:52:57 -0300

    fix carrito

--- src/app/shared/checkout/checkout.component.ts
+++ src/app/shared/checkout/checkout.component.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PedidosService } from '../../services/pedidos.service';
 import { Pedido } from '../../model/pedido.model';
 
-
+declare var MercadoPago: any;
 
 @Component({
   selector: 'app-checkout',
@@ -14,23 +14,64 @@ import { Pedido } from '../../model/pedido.model';
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
-export class CheckoutComponent implements OnInit {
+export class CheckoutComponent implements OnInit, OnChanges {
   paymentMethod: string = '';
   cardholderName: string = '';
   cardNumber: string = '';
   expiryMonth: string = '';
   expiryYear: string = '';
   cvv: string = '';
+  preferenceId: string = '';
 
-  pedido:Pedido=new Pedido(0,0,"","","",[]);
+  pedido: Pedido = new Pedido(0, 0, "", "", "", []);
   constructor(private router: Router, private pedidoService: PedidosService) { }
 
   ngOnInit(): void {
-    this.pedido=this.pedidoService.getPedido();
+    this.pedido = this.pedidoService.getPedido();
+    if (this.paymentMethod === 'mercadopago') {
+      this.initMercadoPagoFlow();
+    }
   }
 
+  ngOnChanges() {
+    if (this.paymentMethod === 'mercadopago') {
+      this.initMercadoPagoFlow();
+    }
+  }
 
+  initMercadoPagoFlow() {
+    fetch('http://localhost:3000/api/crear-preferencia', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        items: this.pedido.carrito?.map((item: any) => ({
+          title: item.producto,
+          quantity: item.cantidad,
+          unit_price: item.precio
+        })) || []
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.preferenceId = data.preferenceId;
+        if (!(window as any).MercadoPago) {
+          const script = document.createElement('script');
+          script.src = 'https://sdk.mercadopago.com/js/v2';
+          script.onload = () => this.initMercadoPago();
+          document.body.appendChild(script);
+        } else {
+          this.initMercadoPago();
+        }
+      });
+  }
 
+  initMercadoPago() {
+    const mp = new MercadoPago('TEST-05849f07-bb39-4db6-8f18-cfb98bd99a98', { locale: 'es-AR' });
+    mp.checkout({
+      preference: { id: this.preferenceId },
+      render: { container: '#mp-checkout', label: 'Pagar con MercadoPago' }
+    });
+  }
 
   onSubmit() {
     if (this.paymentMethod === 'paypal') {
