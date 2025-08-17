d0e9be707d9b800ec7fa9a6d3b3b91f1dce07628
Nicolas Luna <nicolasluna.ca@gmail.com>
2025-08-17 02:20:20 -0300

    daleeeee

---
+++ generate-environment.js
@@ -0,0 +1,3 @@
+// Script dummy para Netlify: genera variables de entorno si es necesario
+console.log('generate-environment.js ejecutado (dummy).');
+// Agrega aquí la lógica real si necesitas escribir archivos de entorno
